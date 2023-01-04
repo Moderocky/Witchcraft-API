e166e601c417f768d33393b3b19a341702acf813
Moderocky <admin@moderocky.com>
2023-01-04 15:49:41 +0000

    Add modifiers.

---
+++ src/main/java/mx/kenzie/witchcraft/data/modifier/Modifier.java
@@ -0,0 +1,13 @@
+package mx.kenzie.witchcraft.data.modifier;
+
+public record Modifier(Type type, double amount, long timeout) {
+    
+    public static Modifier of(Type type, double amount, int ticks) {
+        return new Modifier(type, amount, System.currentTimeMillis() + (50L * ticks));
+    }
+    
+    public enum Type {
+        AMPLITUDE, ARMOUR
+    }
+    
+}
---
+++ src/main/java/mx/kenzie/witchcraft/data/modifier/ModifierMap.java
@@ -0,0 +1,50 @@
+package mx.kenzie.witchcraft.data.modifier;
+
+import com.google.common.util.concurrent.AtomicDouble;
+
+import java.util.HashMap;
+
+public class ModifierMap extends HashMap<String, Modifier> {
+    
+    public static final ModifierMap DEFAULT = new ModifierMap(0);
+    
+    public ModifierMap() {
+    }
+    
+    private ModifierMap(int initialCapacity) {
+        super(initialCapacity);
+    }
+    
+    public double get(Modifier.Type type) {
+        if (this.isEmpty()) return 0;
+        final long time = System.currentTimeMillis();
+        final AtomicDouble count = new AtomicDouble(0);
+        this.values().removeIf(value -> {
+            if (value.timeout() >= time) return true;
+            if (value.type() != type) return false;
+            count.addAndGet(value.amount());
+            return false;
+        });
+        return count.get();
+    }
+    
+    @Override
+    public Modifier get(Object key) {
+        if (this.isEmpty()) return null;
+        final Modifier modifier = super.get(key);
+        if (modifier == null) return null;
+        if (modifier.timeout() < System.currentTimeMillis()) return modifier;
+        this.remove(key, modifier);
+        return null;
+    }
+    
+    @Override
+    public Modifier put(String reason, Modifier modifier) {
+        return super.put(reason, modifier);
+    }
+    
+    public void removeAll(Modifier.Type type) {
+        if (this.isEmpty()) return;
+        this.values().removeIf(value -> value.type() == type);
+    }
+}
