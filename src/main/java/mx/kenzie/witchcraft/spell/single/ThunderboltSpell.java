8a95752df0971d65bf35cc3004209c7188d5a49f
Moderocky <admin@moderocky.com>
2023-01-06 10:19:20 +0000

    Clean-up.

--- src/main/java/mx/kenzie/witchcraft/spell/single/ThunderboltSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/ThunderboltSpell.java
@@ -8,19 +8,19 @@ import org.bukkit.entity.LivingEntity;
 import java.util.Map;
 
 public class ThunderboltSpell extends StandardSpell {
-	public ThunderboltSpell(Map<String, Object> map) {
-		super(map);
-	}
-
-	@Override
-	public boolean canCast(LivingEntity caster) {
-		return true;
-	}
-
-	@Override
-	protected void run(LivingEntity caster, int range, float scale, double amplitude) {
-		final World world = caster.getWorld();
-		final Location location = caster.getEyeLocation();
-		world.strikeLightning(location);
-	}
+    public ThunderboltSpell(Map<String, Object> map) {
+        super(map);
+    }
+    
+    @Override
+    protected void run(LivingEntity caster, int range, float scale, double amplitude) {
+        final World world = caster.getWorld();
+        final Location location = caster.getEyeLocation();
+        world.strikeLightning(location);
+    }
+    
+    @Override
+    public boolean canCast(LivingEntity caster) {
+        return true;
+    }
 }
