e9e25de6efeb59d35e206c7b3240d79eeb3946f6
Moderocky <admin@moderocky.com>
2023-01-09 11:37:05 +0000

    Improve hex visuals.

--- src/main/java/mx/kenzie/witchcraft/spell/single/AbstractTargetedSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/AbstractTargetedSpell.java
@@ -18,15 +18,20 @@ public abstract class AbstractTargetedSpell extends StandardSpell {
         super(map);
     }
     
-    protected Target getTarget(LivingEntity caster, int range) {
+    protected Target getTarget(LivingEntity caster, int range, boolean strikeAir) {
         final World world = caster.getWorld();
         final Location start = caster.getEyeLocation();
         final Vector direction = caster.getLocation().getDirection();
         final RayTraceResult result = world.rayTrace(start, direction, range, FluidCollisionMode.NEVER, true, 0.5, entity -> !entity.equals(caster));
-        if (result == null) return null;
+        if (strikeAir && result == null) return new Target(start.clone().add(direction.clone().multiply(range)), null);
+        else if (result == null) return null;
         final Entity found = result.getHitEntity();
         final Location target = result.getHitPosition().toLocation(world);
         return new Target(target, found);
+    }
+    
+    protected Target getTarget(LivingEntity caster, int range) {
+        return this.getTarget(caster, range, false);
     }
     
     protected record Target(Location target, @Nullable Entity entity) {}
--- src/main/java/mx/kenzie/witchcraft/spell/single/HexSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/HexSpell.java
@@ -1,57 +1,84 @@
 package mx.kenzie.witchcraft.spell.single;
 
 import com.destroystokyo.paper.ParticleBuilder;
-import mx.kenzie.witchcraft.spell.projectile.AbstractProjectile;
-import mx.kenzie.witchcraft.spell.projectile.MagicProjectile;
+import mx.kenzie.witchcraft.WitchcraftAPI;
+import mx.kenzie.witchcraft.spell.effect.ParticleCreator;
+import mx.kenzie.witchcraft.spell.effect.Polygon;
+import mx.kenzie.witchcraft.spell.effect.VectorShape;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.util.Vector;
 
 import java.awt.*;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class HexSpell extends AbstractProjectileSpell {
+public class HexSpell extends AbstractTargetedSpell {
     
-    protected transient final Color color = new Color(117, 53, 152);
+    protected transient final Color color = new Color(147, 18, 183);
     protected transient final ParticleBuilder builder = new ParticleBuilder(Particle.SPELL_MOB)
         .offset(color.getRed() / 255.0, color.getGreen() / 255.0, color.getBlue() / 255.0)
         .count(0)
         .force(true);
+    protected transient final ParticleBuilder dust = new ParticleBuilder(Particle.DRIPPING_OBSIDIAN_TEAR)
+        .count(0).force(true);
     
     public HexSpell(Map<String, Object> map) {
         super(map);
     }
     
     @Override
-    public AbstractProjectile createProjectile(LivingEntity caster, float scale, double amplitude) {
-        final Location location = caster.getEyeLocation();
-        final World world = location.getWorld();
-        final double damage = 3 + amplitude;
-        return new MagicProjectile(caster, location, damage) {
-            @Override
-            public void onTick() {
-                builder.location(this.getLocation()).spawn();
+    public void run(LivingEntity caster, int range, float scale, double amplitude) {
+        final AbstractTargetedSpell.Target trace = this.getTarget(caster, range, true);
+        final Location target = trace.target();
+        final Entity found = trace.entity();
+        final Location eye = caster.getEyeLocation();
+        final Vector direction = eye.getDirection(), reverse = direction.clone().multiply(-2.5);
+        final double damage = 2.5 + amplitude;
+        this.dust.offset(reverse.getX(), reverse.getY(), reverse.getZ());
+        eye.add(direction.clone().multiply(5));
+        final Polygon polygon = ParticleCreator.of(dust).createPolygon(direction, 1.1, 5).fillInLines(true, 0.14);
+        polygon.draw(eye);
+        caster.getWorld().playSound(eye, Sound.ENTITY_WITHER_SHOOT, 0.6F, 1.4F);
+        WitchcraftAPI.executor.submit(() -> {
+            final ParticleCreator creator = ParticleCreator.of(builder);
+            final VectorShape shape = creator.createLine(eye, target, 0.3);
+            for (Vector vector : shape) {
+                final Location point = eye.clone().add(vector);
+                this.builder.location(point).spawn();
+                WitchcraftAPI.sleep(5);
             }
-            
-            @Override
-            public void onLaunch() {
-                world.playSound(location, Sound.ENTITY_WITHER_SHOOT, 0.6F, 1.4F);
-            }
-            
-            @Override
-            public void explode(Location location) {
-                Random random = ThreadLocalRandom.current();
-                world.playSound(location, Sound.BLOCK_GLASS_BREAK, 0.8F, 0.4F);
-                for (int i = 0; i < 6; i++) {
-                    final Location loc = location.clone()
-                        .add(random.nextDouble() - 0.5, random.nextDouble() - 0.5, random.nextDouble() - 0.5);
-                    builder.location(loc).spawn();
-                }
-            }
-        }.setDiameter(0.8);
+            Bukkit.getScheduler().callSyncMethod(WitchcraftAPI.plugin, () -> {
+                final Set<Entity> set = new HashSet<>(target.getNearbyLivingEntities(2));
+                if (found != null) set.add(found);
+                for (Entity entity : set)
+                    WitchcraftAPI.minecraft.damageEntitySafely(entity, caster, damage, EntityDamageEvent.DamageCause.MAGIC);
+                return set.size() > 0;
+            });
+            creator.drawPoof(target, 1.1, 16);
+        });
+    }
+    
+    @Override
+    public boolean canCast(LivingEntity caster) {
+        return true;
+    }
+    
+    
+    private void explode(Location location) {
+        final Random random = ThreadLocalRandom.current();
+        location.getWorld().playSound(location, Sound.BLOCK_GLASS_BREAK, 0.8F, 0.4F);
+        for (int i = 0; i < 6; i++) {
+            final Location loc = location.clone().add(ParticleCreator.random());
+            builder.location(loc).spawn();
+        }
     }
 }
