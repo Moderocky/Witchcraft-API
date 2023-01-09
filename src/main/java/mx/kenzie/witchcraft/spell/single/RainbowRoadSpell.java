e589fc70d1b705e8d11e3d09e37d727b89bb7ea3
Moderocky <admin@moderocky.com>
2023-01-09 13:31:04 +0000

    Add rainbow road spell.

---
+++ src/main/java/mx/kenzie/witchcraft/spell/single/RainbowRoadSpell.java
@@ -0,0 +1,39 @@
+package mx.kenzie.witchcraft.spell.single;
+
+import mx.kenzie.witchcraft.WitchcraftAPI;
+import mx.kenzie.witchcraft.data.entity.CustomEntityType;
+import mx.kenzie.witchcraft.spell.StandardSpell;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Vector;
+
+import java.util.Map;
+
+public class RainbowRoadSpell extends StandardSpell {
+    
+    public RainbowRoadSpell(Map<String, Object> map) {
+        super(map);
+    }
+    
+    @Override
+    public void run(LivingEntity caster, int range, float scale, double amplitude) {
+        final Location point = caster.getLocation().add(0, -1, 0);
+        final Vector direction = point.getDirection().multiply(2);
+        WitchcraftAPI.executor.submit(() -> {
+            for (int i = 0; i < 6; i++) {
+                point.add(direction);
+                if (point.getBlock().getType() != Material.AIR) continue;
+                CustomEntityType.RAINBOW_BRIDGE.spawn(point);
+                caster.getWorld().playSound(point, Sound.BLOCK_AMETHYST_BLOCK_CHIME, 1F, 1F);
+            }
+        });
+    }
+    
+    @Override
+    public boolean canCast(LivingEntity caster) {
+        return true;
+    }
+    
+}
