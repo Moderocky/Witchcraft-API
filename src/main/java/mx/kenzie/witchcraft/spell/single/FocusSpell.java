1d5acdf5fdb2f28b522f74706a44b1da4b7dd422
Moderocky <admin@moderocky.com>
2023-01-10 13:17:47 +0000

    Finish focus spell.

--- src/main/java/mx/kenzie/witchcraft/spell/single/FocusSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/FocusSpell.java
@@ -1,7 +1,7 @@
 package mx.kenzie.witchcraft.spell.single;
 
 import mx.kenzie.witchcraft.WitchcraftAPI;
-import mx.kenzie.witchcraft.spell.StandardSpell;
+import mx.kenzie.witchcraft.entity.Summon;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
@@ -9,13 +9,15 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class FocusSpell extends StandardSpell {
+public class FocusSpell extends AbstractTargetedSpell {
     public FocusSpell(Map<String, Object> map) {
         super(map);
     }
     
     @Override
     public void run(LivingEntity caster, int range, float scale, double amplitude) {
+        final Target target = this.getTarget(caster, range, false);
+        if (!(target.entity() instanceof LivingEntity thing)) return;
         final Set<Entity> entities = new HashSet<>(caster.getLocation().getNearbyEntities(12, 6, 12));
         entities.removeIf(entity -> {
             if (!(entity instanceof LivingEntity living)) return true;
@@ -23,8 +25,10 @@ public class FocusSpell extends StandardSpell {
         });
         for (Entity entity : entities) {
             if (!(entity instanceof LivingEntity living)) continue;
-            if (!WitchcraftAPI.minecraft.isPet(caster, living)) continue;
-            // todo make target
+            final Summon summon = WitchcraftAPI.minecraft.getAsSummon(entity);
+            if (summon == null) continue;
+            if (summon.getOwnerID() != caster.getUniqueId()) continue;
+            summon.setTarget(thing);
         }
     }
     
