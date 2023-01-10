ed2aa2801f824994bc63ceb548f3a8675bdda10e
Moderocky <admin@moderocky.com>
2023-01-10 11:59:30 +0000

    Improve omnipresence with mirror images.

--- src/main/java/mx/kenzie/witchcraft/spell/single/OmnipresenceSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/OmnipresenceSpell.java
@@ -1,10 +1,9 @@
 package mx.kenzie.witchcraft.spell.single;
 
 import com.destroystokyo.paper.ParticleBuilder;
-import com.destroystokyo.paper.profile.PlayerProfile;
 import mx.kenzie.witchcraft.WitchcraftAPI;
-import mx.kenzie.witchcraft.entity.NPC;
 import mx.kenzie.witchcraft.spell.StandardSpell;
+import mx.kenzie.witchcraft.spell.effect.ParticleCreator;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
@@ -13,14 +12,9 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.PlayerInventory;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
 public class OmnipresenceSpell extends StandardSpell {
     
@@ -40,54 +34,22 @@ public class OmnipresenceSpell extends StandardSpell {
         final Location centre = target.getLocation();
         final int count = Math.min(blocks.size(), 3 + (int) amplitude);
         final Random random = ThreadLocalRandom.current();
+        final Set<LivingEntity> entities = new HashSet<>();
         for (int i = 0; i < count; i++) {
-            Block block = blocks.remove(ThreadLocalRandom.current().nextInt(blocks.size()));
-            Location loc = block.getLocation();
-            splash(loc);
-            loc.setDirection(centre.toVector().subtract(loc.toVector()));
-            if (i == 0)
-                player.teleport(loc);
-            else {
-                PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID(), player.getName());
-                profile.setProperties(player.getPlayerProfile().getProperties());
-                final NPC npc = WitchcraftAPI.minecraft.spawnNPC(loc, profile, fake -> {
-                    final PlayerInventory inventory = fake.getInventory();
-                    inventory.setHeldItemSlot(player.getInventory().getHeldItemSlot());
-                    inventory.setContents(player.getInventory().getContents());
-                    inventory.setArmorContents(player.getInventory().getArmorContents());
-                });
-                npc.setOnTick(playerNPC -> {
-                    int nextInt = random.nextInt(40);
-                    if ((nextInt == 19)) {
-                        playerNPC.setYHeadRot((float) (playerNPC.getYHeadRot() + ((random.nextDouble() - 0.5) * 25)));
-                    } else if (nextInt == 20) {
-                        Location l = playerNPC.getBukkitEntity().getLocation();
-                        l.setDirection(target.getLocation().toVector()
-                            .subtract(playerNPC.getBukkitEntity().getLocation().toVector()));
-                        playerNPC.setYHeadRot(l.getYaw());
-                    }
-                });
-                npc.setOnDamage((playerNPC, event) -> {
-                    splash(npc.getBukkitEntity().getLocation());
-                    event.setDamage(0);
-                    playerNPC.discard();
-                });
-                npc.setOnDeath((playerNPC, event) -> {
-                    splash(npc.getBukkitEntity().getLocation());
-                    npc.getBukkitEntity().getInventory().clear();
-                    event.setKeepInventory(true);
-                    event.setKeepLevel(true);
-                    event.deathMessage(null);
-                    event.setShouldDropExperience(false);
-                });
-                WitchcraftAPI.scheduler.schedule(() -> {
-                    if (!npc.getBukkitEntity().isDead()) {
-                        splash(npc.getBukkitEntity().getLocation());
-                        npc.getBukkitEntity().remove();
-                    }
-                }, 12, TimeUnit.SECONDS);
-            }
+            final Block block = blocks.remove(random.nextInt(blocks.size()));
+            final Location location = block.getLocation();
+            this.splash(location);
+            location.setDirection(centre.toVector().subtract(location.toVector()));
+            if (i == 0) player.teleport(location);
+            else entities.add(WitchcraftAPI.minecraft.spawnMirrorImage(location, player));
         }
+        Bukkit.getScheduler().scheduleSyncDelayedTask(WitchcraftAPI.plugin, () -> {
+            for (LivingEntity entity : entities) {
+                if (entity.isDead()) continue;
+                this.splash(entity.getLocation());
+                entity.remove();
+            }
+        }, 9 * 20);
     }
     
     @Override
@@ -103,9 +65,6 @@ public class OmnipresenceSpell extends StandardSpell {
     }
     
     private void splash(Location location) {
-        for (int i = 0; i < 10; i++) {
-            builder.location(location)
-                .spawn();
-        }
+        ParticleCreator.of(builder).drawPoof(location, 1, 10);
     }
 }
