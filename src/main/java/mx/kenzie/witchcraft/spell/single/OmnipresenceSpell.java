b772b181cf59a2e15ecc51e9d486f1c77b8846ba
Moderocky <admin@moderocky.com>
2023-01-10 13:17:54 +0000

    Make omnipresence target.

--- src/main/java/mx/kenzie/witchcraft/spell/single/OmnipresenceSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/OmnipresenceSpell.java
@@ -2,26 +2,22 @@ package mx.kenzie.witchcraft.spell.single;
 
 import com.destroystokyo.paper.ParticleBuilder;
 import mx.kenzie.witchcraft.WitchcraftAPI;
-import mx.kenzie.witchcraft.spell.StandardSpell;
 import mx.kenzie.witchcraft.spell.effect.ParticleCreator;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class OmnipresenceSpell extends StandardSpell {
+public class OmnipresenceSpell extends AbstractTargetedSpell {
     
     protected transient final ParticleBuilder builder = new ParticleBuilder(Particle.DRAGON_BREATH)
         .extra(0.03)
         .force(true);
-    private transient Entity target;
     private transient List<Block> blocks;
     
     public OmnipresenceSpell(Map<String, Object> map) {
@@ -31,18 +27,20 @@ public class OmnipresenceSpell extends StandardSpell {
     @Override
     public void run(LivingEntity caster, int range, float scale, double amplitude) {
         final Player player = ((Player) caster);
-        final Location centre = target.getLocation();
-        final int count = Math.min(blocks.size(), 3 + (int) amplitude);
+        final int count = Math.min(blocks.size(), 2 + (int) amplitude);
         final Random random = ThreadLocalRandom.current();
+        final Location centre = caster.getEyeLocation();
         final Set<LivingEntity> entities = new HashSet<>();
         for (int i = 0; i < count; i++) {
             final Block block = blocks.remove(random.nextInt(blocks.size()));
             final Location location = block.getLocation();
             this.splash(location);
             location.setDirection(centre.toVector().subtract(location.toVector()));
-            if (i == 0) player.teleport(location);
-            else entities.add(WitchcraftAPI.minecraft.spawnMirrorImage(location, player));
+            entities.add(WitchcraftAPI.minecraft.spawnMirrorImage(location, player));
         }
+        final AbstractTargetedSpell.Target target = this.getTarget(caster, range, false);
+        if (target.entity() instanceof LivingEntity living)
+            for (LivingEntity entity : entities) WitchcraftAPI.minecraft.getAsSummon(entity).setTarget(living);
         Bukkit.getScheduler().scheduleSyncDelayedTask(WitchcraftAPI.plugin, () -> {
             for (LivingEntity entity : entities) {
                 if (entity.isDead()) continue;
@@ -55,13 +53,10 @@ public class OmnipresenceSpell extends StandardSpell {
     @Override
     public boolean canCast(LivingEntity caster) {
         final Location location = caster.getEyeLocation();
-        this.target = WitchcraftAPI.minecraft.getTargetEntity(location, 16, 0.96);
-        if (!(target instanceof LivingEntity)) return false;
-        if (!(caster instanceof HumanEntity)) return false;
-        final int count = 8;
-        this.blocks = AbstractTeleportSpell.getValidTeleportSpaces(target.getLocation(), 5.6);
-        this.blocks.removeIf(block -> !WitchcraftAPI.minecraft.hasLineOfSight(block.getLocation(), target.getLocation()));
-        return blocks.size() > count;
+        if (!(caster instanceof Player)) return false;
+        this.blocks = AbstractTeleportSpell.getValidTeleportSpaces(location, 5.6);
+        this.blocks.removeIf(block -> !WitchcraftAPI.minecraft.hasLineOfSight(block.getLocation(), location));
+        return blocks.size() > 6;
     }
     
     private void splash(Location location) {
