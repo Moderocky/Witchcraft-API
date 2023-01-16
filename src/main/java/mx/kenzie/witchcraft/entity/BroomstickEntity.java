a93cf29d2053f34809562a7154653b2a1df34435
Moderocky <admin@moderocky.com>
2023-01-16 10:08:10 +0000

    Pull broomstick methods to API layer.

--- src/main/java/mx/kenzie/witchcraft/entity/BroomstickEntity.java
+++ src/main/java/mx/kenzie/witchcraft/entity/BroomstickEntity.java
@@ -1,11 +1,30 @@
 package mx.kenzie.witchcraft.entity;
 
 import com.destroystokyo.paper.ParticleBuilder;
+import mx.kenzie.witchcraft.spell.effect.ParticleCreator;
 import org.bukkit.entity.LivingEntity;
 
 public interface BroomstickEntity extends Handle {
     
     LivingEntity getBukkitLivingEntity();
+    
+    float getMaxVerticalSpeed();
+    
+    void setMaxVerticalSpeed(float maxVerticalSpeed);
+    
+    float getMinSpeed();
+    
+    void setMinSpeed(float minSpeed);
+    
+    float getMaxSpeed();
+    
+    void setMaxSpeed(float maxSpeed);
+    
+    float getMaxStrafe();
+    
+    void setMaxStrafe(float maxStrafe);
+    
+    ParticleCreator getCreator();
     
     void setRainbow(boolean rainbow);
     
