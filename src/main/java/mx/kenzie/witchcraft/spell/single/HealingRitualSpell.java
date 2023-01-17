91059be71d2da07e2f5c19aab1ae9bc958533982
Moderocky <admin@moderocky.com>
2023-01-17 13:16:01 +0000

    Clean up healing ritual.

--- src/main/java/mx/kenzie/witchcraft/spell/single/HealingRitualSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/HealingRitualSpell.java
@@ -1,10 +1,12 @@
 package mx.kenzie.witchcraft.spell.single;
 
-import com.destroystokyo.paper.ParticleBuilder;
+import mx.kenzie.witchcraft.Minecraft;
 import mx.kenzie.witchcraft.WitchcraftAPI;
 import mx.kenzie.witchcraft.spell.StandardSpell;
-import org.bukkit.*;
-import org.bukkit.attribute.Attribute;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Vibration;
+import org.bukkit.World;
 import org.bukkit.entity.LivingEntity;
 
 import java.util.ArrayList;
@@ -13,38 +15,36 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class HealingRitualSpell extends StandardSpell {
-	protected transient final ParticleBuilder builder = new ParticleBuilder(Particle.VILLAGER_HAPPY)
-			.count(0)
-			.force(true);
-
-	public HealingRitualSpell(Map<String, Object> map) {
-		super(map);
-	}
-
-	@Override
-	public boolean canCast(LivingEntity caster) {
-		return true;
-	}
-
-	@Override
-	protected void run(LivingEntity caster, int range, float scale, double amplitude) {
-		final Location centre = caster.getEyeLocation();
-		final World world = caster.getWorld();
-		final List<LivingEntity> entities = new ArrayList<>(centre.getNearbyLivingEntities(10, 5));
-		entities.removeIf(target -> !WitchcraftAPI.minecraft.isAlly(caster, target));
-		for (LivingEntity entity : entities) {
-			if (entity == caster)
-				continue;
-			Vibration vibration = new Vibration(new Vibration.Destination.EntityDestination(entity), 40);
-			for (int i = 0; i < 250; i += 50) {
-				WitchcraftAPI.scheduler.schedule(() ->
-								world.spawnParticle(Particle.VIBRATION, centre, 1, vibration),
-						i, TimeUnit.MILLISECONDS);
-			}
-		}
-		WitchcraftAPI.scheduler.schedule(() -> {
-			for (LivingEntity entity : entities)
-				entity.setHealth(Math.min(entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue(), entity.getHealth() + 5));
-		}, 2, TimeUnit.SECONDS);
-	}
+    
+    public HealingRitualSpell(Map<String, Object> map) {
+        super(map);
+    }
+    
+    @Override
+    public boolean canCast(LivingEntity caster) {
+        return true;
+    }
+    
+    @Override
+    protected void run(LivingEntity caster, int range, float scale, double amplitude) {
+        final Minecraft minecraft = Minecraft.getInstance();
+        final Location centre = caster.getEyeLocation();
+        final World world = caster.getWorld();
+        final double amount = 4 + amplitude;
+        final List<LivingEntity> entities = new ArrayList<>(centre.getNearbyLivingEntities(10, 5));
+        entities.removeIf(target -> !minecraft.isAlly(caster, target));
+        for (int i = 0; i < 5; i++) {
+            WitchcraftAPI.scheduler.schedule(() -> {
+                for (LivingEntity entity : entities) {
+                    if (entity.isDead()) continue;
+                    final Vibration vibration = new Vibration(new Vibration.Destination.EntityDestination(entity), 40);
+                    world.spawnParticle(Particle.VIBRATION, centre, 1, vibration);
+                }
+            }, i * 380, TimeUnit.MILLISECONDS);
+        }
+        WitchcraftAPI.scheduler.schedule(() -> {
+            for (LivingEntity entity : entities)
+                entity.setHealth(Math.min(minecraft.getMaxHealth(entity), entity.getHealth() + amount));
+        }, 2, TimeUnit.SECONDS);
+    }
 }
