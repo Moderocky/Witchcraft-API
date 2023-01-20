2ea23de3fbd5606995e5c2cdac13963d0750da47
Moderocky <admin@moderocky.com>
2023-01-20 16:39:50 +0000

    Fix bone cage.

--- src/main/java/mx/kenzie/witchcraft/spell/single/AbstractEntitySpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/AbstractEntitySpell.java
@@ -1,23 +1,24 @@
 package mx.kenzie.witchcraft.spell.single;
 
-import mx.kenzie.witchcraft.spell.StandardSpell;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
 import java.util.Map;
 
-public abstract class AbstractEntitySpell extends StandardSpell {
-	public AbstractEntitySpell(Map<String, Object> map) {
-		super(map);
-	}
-
-	protected transient LivingEntity target;
-
-	@Override
-	public boolean canCast(LivingEntity caster) {
-		final Entity entity = caster.getTargetEntity(20);
-		if (!(entity instanceof LivingEntity living)) return false;
-		target = living;
-		return true;
-	}
+public abstract class AbstractEntitySpell extends AbstractTargetedSpell {
+    public AbstractEntitySpell(Map<String, Object> map) {
+        super(map);
+    }
+    
+    protected transient LivingEntity target;
+    
+    @Override
+    public boolean canCast(LivingEntity caster) {
+        final Target target = this.getTarget(caster, 25);
+        if (target == null) return false;
+        final Entity entity = target.entity();
+        if (!(entity instanceof LivingEntity living)) return false;
+        this.target = living;
+        return true;
+    }
 }
--- src/main/java/mx/kenzie/witchcraft/spell/single/BoneCageSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/BoneCageSpell.java
@@ -1,9 +1,11 @@
 package mx.kenzie.witchcraft.spell.single;
 
-import com.destroystokyo.paper.ParticleBuilder;
+import mx.kenzie.witchcraft.WitchcraftAPI;
 import mx.kenzie.witchcraft.spell.effect.ParticleCreator;
+import mx.kenzie.witchcraft.spell.effect.VectorShape;
 import org.bukkit.Location;
-import org.bukkit.Particle;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -12,25 +14,28 @@ import org.bukkit.util.Vector;
 import java.util.Map;
 
 public class BoneCageSpell extends AbstractEntitySpell {
-	protected transient final ParticleCreator creator = ParticleCreator.of(new ParticleBuilder(Particle.ASH)
-			.count(0));
-	public BoneCageSpell(Map<String, Object> map) {
-		super(map);
-	}
-
-	@Override
-	public boolean canCast(LivingEntity caster) {
-		return super.canCast(caster);
-	}
-
-	@Override
-	protected void run(LivingEntity caster, int range, float scale, double amplitude) {
-		if (target == null) throw new RuntimeException();
-		final Location centre = target.getLocation();
-		target.setVelocity(new Vector(0, 0, 0));
-		final int duration = (int) (5 + (amplitude * 2));
-		target.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, duration, 128));
-		target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, duration, 128));
-		creator.playSpiral(centre, 1.5, 2.3, 12, 2);
-	}
+    protected transient final ParticleCreator creator = ParticleCreator.of(Material.BONE_BLOCK);
+    protected transient final VectorShape circle = creator.createCircle(new Vector(0, 1, 0), 0.8, 26);
+    
+    public BoneCageSpell(Map<String, Object> map) {
+        super(map);
+    }
+    
+    @Override
+    protected void run(LivingEntity caster, int range, float scale, double amplitude) {
+        if (target == null) throw new RuntimeException();
+        final Location centre = target.getLocation().add(0, 0.2, 0);
+        this.target.setVelocity(new Vector(0, 0, 0));
+        final int duration = (int) (5 + (amplitude * 2)) * 20;
+        this.target.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, duration, 128, false, false, false));
+        this.target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, duration, 128, false, false, false));
+        centre.getWorld().playSound(centre, Sound.BLOCK_BONE_BLOCK_BREAK, 1.2F, 1.0F);
+        WitchcraftAPI.executor.submit(() -> {
+            for (int i = 1; i < 8; i++) {
+                centre.add(0, 0.2, 0);
+                this.creator.draw(centre, circle);
+                WitchcraftAPI.sleep(100);
+            }
+        });
+    }
 }
