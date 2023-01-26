1bde3fe2727a0bf1c52012b52d1358e044f81a07
Moderocky <admin@moderocky.com>
2023-01-26 13:24:23 +0000

    Add counteract spell.

---
+++ src/main/java/mx/kenzie/witchcraft/spell/single/CounteractSpell.java
@@ -0,0 +1,52 @@
+package mx.kenzie.witchcraft.spell.single;
+
+import mx.kenzie.witchcraft.Minecraft;
+import mx.kenzie.witchcraft.Session;
+import mx.kenzie.witchcraft.WitchcraftAPI;
+import mx.kenzie.witchcraft.entity.FloatingBlock;
+import mx.kenzie.witchcraft.entity.Projectile;
+import mx.kenzie.witchcraft.spell.StandardSpell;
+import mx.kenzie.witchcraft.spell.effect.ParticleCreator;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CounteractSpell extends StandardSpell {
+    protected transient final ParticleCreator creator = ParticleCreator.of(Particle.END_ROD.builder().count(1));
+    
+    public CounteractSpell(Map<String, Object> map) {
+        super(map);
+    }
+    
+    @Override
+    public boolean canCast(LivingEntity caster) {
+        return true;
+    }
+    
+    @Override
+    protected void run(LivingEntity caster, int range, float scale, double amplitude) {
+        final List<Entity> entities = new ArrayList<>(caster.getNearbyEntities(range, range / 2F, range));
+        final Minecraft minecraft = Minecraft.getInstance();
+        for (Entity entity : entities) {
+            if (entity == caster) continue;
+            if (minecraft.isAlly(entity, caster)) continue;
+            if (entity instanceof Projectile projectile) projectile.remove();
+            else if (entity instanceof FloatingBlock block) block.remove();
+            else if (entity instanceof Player player) {
+                final Session session = WitchcraftAPI.plugin.get(player);
+                if (session != null) session.resetCast();
+            }
+        }
+        this.creator.createCircle(new Vector(0, -1, 0), 5, 160).draw(caster.getEyeLocation(), 10);
+        // todo limit this to level 5 spells
+        caster.getWorld().playSound(caster.getLocation(), Sound.BLOCK_BEACON_DEACTIVATE, 1.0F, 1.5F);
+    }
+    
+}
