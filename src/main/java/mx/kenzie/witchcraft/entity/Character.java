61b4c9519160c188836608fad70f16d83c6f2cfd
Moderocky <admin@moderocky.com>
2023-01-27 09:42:28 +0000

    Add archetype getter.

--- src/main/java/mx/kenzie/witchcraft/entity/Character.java
+++ src/main/java/mx/kenzie/witchcraft/entity/Character.java
@@ -2,10 +2,13 @@ package mx.kenzie.witchcraft.entity;
 
 import mx.kenzie.witchcraft.entity.goal.Behaviour;
 import mx.kenzie.witchcraft.entity.goal.CharacterGoal;
+import mx.kenzie.witchcraft.npc.CharacterArchetype;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.NPC;
 
 public interface Character extends NPC, CustomEntity, LivingEntity, Active {
+    
+    CharacterArchetype getArchetype();
     
     boolean isNamed();
     
