9b639af4c87acb4104dba96c1b606a07b5f1a233
Moderocky <admin@moderocky.com>
2023-01-28 12:34:08 +0000

    Add simple flags system.

---
+++ src/main/java/mx/kenzie/witchcraft/Flags.java
@@ -0,0 +1,68 @@
+package mx.kenzie.witchcraft;
+
+import org.bukkit.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public enum Flags {
+
+    ;
+
+    private static final Map<Entity, FlagSet> MAP = new WeakHashMap<>();
+    private static final FlagSet EMPTY = new EmptyFlagSet();
+
+    public static @NotNull FlagSet of(Entity entity) {
+        MAP.putIfAbsent(entity, new FlagSet());
+        return MAP.getOrDefault(entity, EMPTY);
+    }
+
+    public static boolean clean() {
+        return MAP.values().removeIf(FlagSet::isEmpty);
+    }
+
+    public static class FlagSet extends HashSet<Flags> {
+
+        public boolean has(Flags... flags) {
+            for (Flags flag : flags) if (!this.contains(flag)) return false;
+            return true;
+        }
+
+        public boolean hasAny(Flags... flags) {
+            for (Flags flag : flags) if (this.contains(flag)) return true;
+            return false;
+        }
+
+    }
+
+    private static class EmptyFlagSet extends FlagSet {
+        @Override
+        public boolean add(Flags flags) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(@NotNull Collection<? extends Flags> c) {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(@NotNull Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+    }
+
+}
