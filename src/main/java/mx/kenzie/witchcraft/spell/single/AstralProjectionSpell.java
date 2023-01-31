3262f31beef785dab5006a6e5dfca3ee10801554
Moderocky <admin@moderocky.com>
2023-01-31 10:39:53 +0000

    Improve astral projection.

--- src/main/java/mx/kenzie/witchcraft/spell/single/AstralProjectionSpell.java
+++ src/main/java/mx/kenzie/witchcraft/spell/single/AstralProjectionSpell.java
@@ -8,10 +8,11 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AstralProjectionSpell extends StandardSpell {
     public AstralProjectionSpell(Map<String, Object> map) {
@@ -26,15 +27,23 @@ public class AstralProjectionSpell extends StandardSpell {
     @Override
     protected void run(LivingEntity caster, int range, float scale, double amplitude) {
         if (!(caster instanceof Player player)) return;
-        final int lifetime = (int) Math.floor(10 + (amplitude * 2));
+        final int lifetime = (int) Math.floor(8 + (amplitude * 2));
         final Location start = player.getLocation();
         final Minecraft minecraft = Minecraft.getInstance();
         final LivingEntity mirror = Minecraft.getInstance().spawnMirrorImageNoAI(start, player);
         mirror.setHealth(Math.min(caster.getHealth(), minecraft.getMaxHealth(mirror)));
         player.setGameMode(GameMode.SPECTATOR);
-        final AtomicBoolean running = new AtomicBoolean(true);
+        final Listener listener = new Listener() {
+            @EventHandler
+            public void event(PlayerTeleportEvent event) {
+                if (event.getPlayer() != player) return;
+                if (event.getCause() != PlayerTeleportEvent.TeleportCause.SPECTATE) return;
+                event.setCancelled(true);
+            }
+        };
+        Bukkit.getPluginManager().registerEvents(listener, WitchcraftAPI.plugin);
         final Runnable end = () -> {
-            if (!running.get()) return;
+            PlayerTeleportEvent.getHandlerList().unregister(listener);
             player.setGameMode(GameMode.ADVENTURE);
             if (mirror.isDead()) {
                 player.damage(50);
@@ -43,17 +52,7 @@ public class AstralProjectionSpell extends StandardSpell {
             player.setHealth(Math.min(minecraft.getMaxHealth(player), mirror.getHealth()));
             player.teleport(mirror);
             mirror.remove();
-            running.set(false);
         };
-        WitchcraftAPI.scheduler.scheduleAtFixedRate(() -> {
-            if (!player.isOnline()) throw new RuntimeException();
-            if (player.getGameMode() != GameMode.SPECTATOR) throw new RuntimeException();
-            if (mirror.isDead() || player.getWorld() != start.getWorld() || player.getLocation()
-                .distanceSquared(start) > (30 * 30)) {
-                Bukkit.getScheduler().runTask(WitchcraftAPI.plugin, end);
-                throw new RuntimeException();
-            }
-        }, 300, 100, TimeUnit.MILLISECONDS);
         Bukkit.getScheduler().scheduleSyncDelayedTask(WitchcraftAPI.plugin, end, lifetime * 20L);
     }
 
